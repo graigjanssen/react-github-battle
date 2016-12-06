854f3ed5a1b8500c90491e75a55a42c57f3356ff
Graig Janssen <graig.janssen@gmail.com>
2016-12-06 16:21:44 -0500

    Add first two routes and components

---
+++ app/config/routes.js
@@ -0,0 +1,10 @@
+// React Router maps components to URLs
+
+var React = require('react');
+// installed react-router with npm
+var ReactRouter = require('react-router');
+
+// Make these objects from React Router
+var Router = ReactRouter.Router;
+var Route = ReactRouter.Route;
+var IndexRoute = ReactRouter.IndexRoute;
