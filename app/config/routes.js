10bb2a6374fbe51b6acc4c0973be30a91d4a5013
Graig Janssen <graig.janssen@gmail.com>
2016-12-09 14:41:59 -0500

    Add styles object

--- app/config/routes.js
+++ app/config/routes.js
@@ -9,17 +9,22 @@ var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
 var IndexRoute = ReactRouter.IndexRoute;
 var hashHistory = ReactRouter.hashHistory;
+
+// Components
 var Main = require('../components/Main');
 var Home = require('../components/Home');
+var PromptContainer = require('../containers/PromptContainer');
 
 // React-router objects behave as components.  Main component will be parent route, will render views that stay the same on every page like header, nav menu, footer. //
 
 //Note: (), not {}
-//Note: Could not load until added history property to Router 
+//Note: Could not load until added history property to Router
 var routes = (
   <Router history={hashHistory}>
     <Route path='/' component={Main}>
-      <Route path='/home' component={Home}/>
+      <IndexRoute component={Home}/>
+      <Route path="playerOne" header="Player One" component={PromptContainer}/>
+      <Route path="playerTwo/:playerOne" header="Player Two" component={PromptContainer}/>
     </Route>
   </Router>
 );
